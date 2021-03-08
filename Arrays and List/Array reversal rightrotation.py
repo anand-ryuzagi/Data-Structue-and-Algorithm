4742d87c923b9526d262e6e970eb189f8fdf9d84
Anand Prasad Singh <78860832+anand-ryuzagi@users.noreply.github.com>
2021-03-08 11:38:52 +0530

    Update Array reversal rightrotation.py

--- Arrays and List/Array reversal rightrotation.py
+++ Arrays and List/Array reversal rightrotation.py
@@ -1,5 +1,12 @@
 #time complexity = O(n)
+
 def rotate(arr,n,d):
+    d= d%n #when number of rotation is larger than array length
+
+    #to handle negative value as some negative rotation value will be equal to a positive rotation value by d+l
+    if d<0:
+        d = d+n
+
     reverseArray(arr,0,n-d-1)
     reverseArray(arr,n-d,n-1)
     reverseArray(arr,0,n-1)
@@ -14,5 +21,5 @@ def reverseArray(arr,start,end):
 
 arr = [1,2,3,4,5,6,7]
 print(f"array before rotation {arr}")
-rotate(arr,7,2)
+rotate(arr,7,-2)
 print(f"array after rotation {arr}")
